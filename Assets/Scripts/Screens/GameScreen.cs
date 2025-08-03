d57c805719c09226c48b5e8224ee022eb600e66b
Gaurav garg <gauravthailand@gmail.com>
2025-08-03 17:53:30 +0100

    - adding tick when killed all enemies - and Joystick movement

--- Assets/Scripts/Screens/GameScreen.cs
+++ Assets/Scripts/Screens/GameScreen.cs
@@ -5,16 +5,25 @@ public class GameScreen : MonoBehaviour
 {
     private const string ENEMY_COUNT = "x {0}";
     
+    [SerializeField] private GameObject m_joystickVisuals;
     [SerializeField] private Text m_enemyCount;
+    [SerializeField] private GameObject m_tickGameObject;
 
     private void Awake()
     {
         GameplayEvents.EnemyCountUpdated += GameplayEventsOnEnemyCountUpdated;
+        m_tickGameObject.SetActive(false);
     }
 
     private void Start()
     {
         m_enemyCount.text = string.Format(ENEMY_COUNT, EnemyManager.Instance.GetEnemyCount());
+        
+        Transform player = GameManager.Instance.GetPlayer();
+        if (player != null)
+        {
+            player.GetComponent<VirtualJoystick>().RegisterUIVisuals(m_joystickVisuals);
+        }
     }
 
     private void OnDestroy()
@@ -24,6 +33,16 @@ public class GameScreen : MonoBehaviour
     
     private void GameplayEventsOnEnemyCountUpdated(int count)
     {
-        m_enemyCount.text = string.Format(ENEMY_COUNT, count);
+        if (count <= 0)
+        {
+            m_tickGameObject.SetActive(true);
+            m_enemyCount.gameObject.SetActive(false);
+        }
+        else
+        {
+            m_tickGameObject.SetActive(false);
+            m_enemyCount.gameObject.SetActive(true);
+            m_enemyCount.text = string.Format(ENEMY_COUNT, count);    
+        }
     }
 }
